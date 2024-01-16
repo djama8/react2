bdb84b864cbf7a3aa07dd9be461914d518d47bd4
djama <8djama8@mail.ru>
2024-01-16 17:42:14 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { useState } from 'react';
 import './App.css';
+import Calculator from './components/calculator/calculator';
 import Header from './components/header/header';
 import Main from './components/main/main';
 
@@ -19,11 +20,14 @@ function App() {
 
   return (
     <div className="App">
+     
       <Header  links={links}  bgcolor="#fff"/>
       <Main  img="https://s0.bloknot-krasnodar.ru/thumb/850x0xcut/upload/iblock/012/13c9210826f1_99bbac589a85f89f4258d169306993e8_887.jpg" text="Агаева Джамала" button="Подписаться"/>
       <h1>{counter}</h1>
       <button onClick={() => plus()}>+</button>
       <button onClick={() => minus()}>-</button>
+
+      <Calculator />
     </div>
   );
 }
